8053e2cf5f1ed2cd6ec3f1fa4980e6917c51b84b
AinhoaCad <124732250+AinhoaCad@users.noreply.github.com>
2023-03-07 20:18:28 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Main from './src/components/Main';
+
+
+export default function App() {
+  return <Main/ >
+}
