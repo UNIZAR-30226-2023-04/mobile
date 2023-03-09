bf56788ab6e97cff2d760b1698066a313b4f9151
AinhoaCad <124732250+AinhoaCad@users.noreply.github.com>
2023-03-09 16:15:55 +0100

    Delete App.js

--- App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Main from './src/components/Main';
-
-
-export default function App() {
-  return <Main/ >
-}
